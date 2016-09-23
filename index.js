3aa774ec27a205698dd4133f9c5f3c7b9566cb18
damian <damian@damian.local>
2016-09-22 23:46:29 -0500

    update balance command

--- index.js
+++ index.js
@@ -47,12 +47,12 @@ tg.router
       var info = JSON.stringify({
         "firstname": nombre,
         "lastname": apellido,
-        "TelegramUserName": username,
-        "telegramId": telegramId
+        "username": username,
+        "idTelegram": idTelegram
       });
       request.post({
         type: "POST",
-        url: 'http://api.minka.io:8081/telegram/registro',
+        url: 'http://api.minka.io:8082/telegram/registro',
         headers: {
           "content-type" : "application/json"
         },
@@ -61,156 +61,12 @@ tg.router
       });
     })
   })
-
-  tg.controller('LoginController', (res) => {
-    tg.for('login', ($) => {
-      var form = {
-          codigo: {
-              q: 'Escribe tu codigo',
-              error: 'Lo siento, ingresaste un valor incorrecto',
-              validator: (input, callback) => {
-                  if(input['text']) {
-                      callback(true)
-                      return
-                  }
-                  callback(false)
-              }
-          },
-          confirmCodigo: {
-              q: 'Confirma el codigo',
-              error: 'Lo siento, ingresaste un valor incorrecto',
-              validator: (input, callback) => {
-                  if(input['text']) {
-                      callback(true)
-                      return
-                  }
-                  callback(false)
-              }
-          }
-        }
-        $.runForm(form, (result) => {
-          if(result.codigo != result.confirmCodigo){
-            $.sendMessage("los codigos no coinciden vuelve a intentarlo");
-            return;
-          }
-          var info = JSON.stringify({
-            "telegramId": $.user.id,
-            "code": result.codigo
-          });
-          request.post({
-            type: "POST",
-            url: 'http://api.minka.io:8082/telegram/loginMislukas',
-            headers: {
-              "content-type": "application/json",
-            },
-            body: info,
-            dataType: 'json'
-            }, function(err, response, body){
-              var datos = JSON.parse(body);
-              if(err){
-                console.log(err);
-                return;
-              }else if(datos.verificado){
-                login = true;
-                nombre = datos.nombre;
-                apellido = datos.apellido;
-                phone = datos.phone;
-                balance = datos.balance;
-                minkaId = datos.id;
-                telegramId = datos.idTelegram;
-                $.sendMessage('Gracias ' + nombre  + ' ahora puedes utilizar todos los servicios de mislukasbot');
-              }else{
-                send = false;
-                $.sendMessage(datos.message);
-              }
-            });
-        })
-    })
-  })
-
-  tg.controller('RegistroController', (res) => {
-    tg.for('registro', ($) => {
-      var form = {
-          phone: {
-              q: 'Cual es tu numero de celular',
-              error: 'Lo siento, ingresaste un valor incorrecto',
-              validator: (input, callback) => {
-                  if(input['text']) {
-                      callback(true)
-                      return
-                  }
-                  callback(false)
-              }
-          },
-          code: {
-              q: 'Ingresa un pin',
-              error: 'Lo siento, ingresaste un valor incorrecto',
-              validator: (input, callback) => {
-                  if(input['text']) {
-                      callback(true)
-                      return
-                  }
-                  callback(false)
-              }
-          },
-          confirmPin: {
-            q: 'Confirma el pin',
-            error: 'Lo siento, ingresaste un valor incorrecto',
-            validator: (input, callback) => {
-                if(input['text']) {
-                    callback(true)
-                    return
-                }
-                callback(false)
-            }
-          }
-        }
-        $.runForm(form, (result) => {
-          if(result.code != result.confirmPin){
-            $.sendMessage("los codigos no coinciden vuelve a intentarlo");
-            return;
-          }
-          nombre = $.user.first_name;
-          apellido = $.user.last_name;
-          telegramId = $.user.id;
-          phone = result.phone;
-          code = result.code;
-          var info = JSON.stringify({
-            "firstname": nombre,
-            "lastname": "fdsaf",
-            "phone": phone,
-            "telegramId": telegramId,
-            "code": code
-          });
-          request.post({
-            type: "POST",
-            url: 'http://api.minka.io:8081/telegram/registro',
-            headers: {
-              "content-type" : "application/json"
-            },
-            body: info,
-            dataType: 'json'
-          }, function(err, response, body){
-            var datos = JSON.parse(body);
-            if(err){
-              console.log(err)
-            }else if(!datos.Person) {
-              $.sendMessage($.user.first_name + " te has registrado con exito.");
-            }else if(datos.Person){
-              $.sendMessage("El usuario ya existe en la app");
-            }else{
-              $.sendMessage("Algo ha salido mal vuelve a hacer el proceso con el comando /registro");
-            }
-            });
-        })
-  })
-})
+  
 tg.controller('SaldoController', (res) => {
   tg.for('saldo', ($) => {
-    if(login == true){
       request.get({
         type: "GET",
-        url: 'http://api.minka.io:8082/person/'+minkaId+'/balance',
+        url: 'http://api.minka.io:8082/person/'+$.user.id+'/balance',
         headers: {
           "content-type": "application/json",
         },
@@ -221,12 +77,9 @@ tg.controller('SaldoController', (res) => {
         if(err){
           console.log(err)
         }else if(datos.wallet){
-          $.sendMessage(nombre +" tu saldo en estos momentos es de " + saldo.balance + " LUK");
+          $.sendMessage($.user.first_name +" tu saldo en estos momentos es de " + saldo.balance + " LUK");
         }
       });
-    }else{
-      $.sendMessage("Aun no te has logueado y si no me dejas saber quien eres no te puedo ayudar :c, dime quien eres con el comando /login");
-    }
   })
 })
 
@@ -300,9 +153,7 @@ tg.controller('HelpController', ($) => {
   tg.for('help', ($) => {
     $.sendMessage("hola, gracias por contar conmigo\n"+
       "puedes utilizar los siguientes comandos para hablar conmigo\n"+
-      "/registro (:telefono :codigo) \t crea una cuenta en nuestra app\n"+
-      "/login (:codigo)\t\t ingresa a la app\n"+
-      "/enviar (:telefono :valor)\t envia dinero a otra cuenta\n"+
+      "/enviar \t envia dinero a otra cuenta\n"+
       "/saldo \t consulta tu saldo\n"+
       "/help \t menu de comandos\n"+
       "Espero poder ayudarte en todo lo que necesites");
